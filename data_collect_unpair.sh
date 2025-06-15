1ba90933e8b9612a9ccce0394dd9c37980de881d
Rajat Sethi <rajatsethi2001@gmail.com>
2025-06-15 15:46:58 -0400

    Tuberculosis

--- data_collect_unpair.sh
+++ data_collect_unpair.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
+ulimit -v $((4000 * 1024))
 set -x
-runs=("SRR12841967" "SRR12841966" "SRR12841965")
-output_dir="Aneurysm"
+runs=("SRR10401138")
+output_dir="Lupus"
+file_limit=500000000
+compress_factor=5
 
 mkdir -p ${output_dir}
 rm -rf tmp
-for run in "${runs[@]}"; do
+sample_spots=100000
+true_file_limit=$(($compress_factor * $file_limit))
+for acc in "${runs[@]}"; do
     mkdir -p tmp
-    run_name="${run// /_}"
-    touch tmp/${run_name}.fastq
-    for acc in $run; do
-        fasterq-dump -p -O tmp ${acc}
-        if [ "tmp/${acc}.fastq" != "tmp/${run_name}.fastq" ]; then
-            cat tmp/${acc}.fastq >> tmp/${run_name}.fastq
-        fi
+    fastq-dump -N 1 -X ${sample_spots} -O tmp ${acc}
+    sample_bytes=$(du -b tmp/${acc}.fastq | awk '{sum += $1} END {print sum}')
+    total_spots=$(($true_file_limit * $sample_spots / $sample_bytes))
+    
+    fastq-dump -N 1 -X ${total_spots} -O tmp ${acc} &
+    DUMP_PID=$!
+
+    set +x
+    while kill -0 $DUMP_PID 2>/dev/null; do
+        bytes=$(du -b tmp/${acc}.fastq 2>/dev/null | awk '{sum += $1} END {print sum}')
+        percent=$((100 * $bytes / $true_file_limit))
+        echo "[`date +%T`] Estimated progress: $percent% ($bytes / $true_file_limit Bytes)"
+        sleep 10
     done
-    round_million=$(awk -v lines=$(wc -l < tmp/${run_name}.fastq) \
-                      -v bytes=$(stat -c %s tmp/${run_name}.fastq) \
-                      'BEGIN {
-                          val = (lines / bytes) * (500 * 1024 * 1024) * 5;
-                          rounded = int((val + 500000) / 1000000) * 1000000;
-                          print rounded
-                      }')
-    head -n ${round_million} tmp/${run_name}.fastq > tmp/${run_name}_half.fastq
-    mv tmp/${run_name}_half.fastq tmp/${run_name}.fastq
-    fastp -i tmp/${run_name}.fastq -o tmp/${run_name}_trim.fq -j tmp/fastp.json -h tmp/fastp.html
-    salmon quant -i salmon_index -l A -r tmp/${run_name}_trim.fq -o tmp --validateMappings
-    python tx2gene.py -p tmp/quant.sf -o ${output_dir}/${run_name}.csv
+    set -x
+
+    fastp -i tmp/${acc}.fastq -o tmp/${acc}_trim.fq -j tmp/fastp.json -h tmp/fastp.html
+    salmon quant -i salmon_index -l A -r tmp/${acc}_trim.fq -o tmp --validateMappings
+    python tx2gene.py -p tmp/quant.sf -o ${output_dir}/${acc}.csv
     rm -rf tmp
 done
