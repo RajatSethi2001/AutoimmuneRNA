503a7f7a5bba380c0e30327aa41f6677e5c3d3c5
Rajat Sethi <rajatsethi2001@gmail.com>
2025-09-03 02:37:19 -0400

    Updated important genes and condition enrichment

--- condition_enrichment.py
+++ condition_enrichment.py
@@ -1,38 +1,80 @@
 import gseapy as gp
 import mygene
+import numpy as np
+import os
 import pandas as pd
+import random
 import torch
-from train_condition_model import ConditionModelLinear
+from scipy.stats import mannwhitneyu
+from statsmodels.stats.multitest import multipletests
+from utils import get_zscores, get_zscore_minmax
 
-condition_of_interest = "Breast_Cancer"
-savefile = "linear_model_no_bias.pth"
+def main():
+    with open("Data/important_genes.txt", "r") as f:
+        gene_ids = f.read().splitlines()
 
-checkpoint = torch.load(savefile, weights_only=True)
-genes = checkpoint["genes"]
-conditions = checkpoint["conditions"]
-model = ConditionModelLinear(len(genes), len(conditions))
-model = torch.compile(model)
-model.load_state_dict(checkpoint["model_state_dict"])
+    condition_parent_dir = "Conditions"
+    condition_of_interest = "Hidradenitis_Supparativa"
+    other_conditions = ["Amyotrophic_Lateral_Sclerosis", "Lupus", "Parkinson", "Podoconiosis", "Primary_Sclerosing_Cholangitis", "Ulcerative_Colitis", "Shingles", "Sepsis", "Scleroderma", "MRSA_Bacteremia", "Crohns_Disease", "Acute_Pancreatitis", "Aneurysm", "Tuberculosis", "Acute_Myeloid_Leukemia", "Endocarditis", "Schistosomiasis", "Leprosy", "Chronic_Myeloid_Leukemia", "Dengue", "Alzheimer", "Coronary_Artery_Disease", "COPD", "Breast_Cancer", "Crimean_Congo_Hemorrhagic_Fever", "Hypertension,Drug_Abuse", "Hypertension", "COVID19", "Depression", "PTSD", "HIV", "HIV,Tuberculosis", "Malaria", "SFTS", "Cystic_Fibrosis", "Chikungunya", "Rheumatoid_Arthritis", "Polycystic_Kidney_Disease", "Myelofibrosis"]
 
-weights = model.get_network_weights()
-condition_index = conditions.index(condition_of_interest)
-condition_weights = weights[condition_index].tolist()
-condition_weight_pairs = list(zip(genes, condition_weights))
-condition_weight_pairs.sort(key = lambda x: abs(x[1]), reverse=True)
+    condition_df = pd.DataFrame()
+    path = f"{condition_parent_dir}/{condition_of_interest}"
+    print(f"Processing Directory: {path}")
+    filenames = os.listdir(path)
+    random.shuffle(filenames)
 
-ensembl_ids = [x[0].split(".")[0] for x in condition_weight_pairs]
-mg = mygene.MyGeneInfo()
-query_result = mg.querymany(ensembl_ids, scopes='ensembl.gene', fields='symbol', species='human')
-id_to_symbol = {item['query']: item.get('symbol', None) for item in query_result}
-gene_symbols = list(filter(None, id_to_symbol.values()))
-expr_values = [x[1] for x in condition_weight_pairs]
+    for filename in filenames:
+        csv_path = f"{path}/{filename}"
+        df = pd.read_csv(csv_path, index_col=0)
+        df = np.log2(df + 1)
+        df = df.apply(get_zscores, axis=0)
+        df = df.apply(get_zscore_minmax, axis=0)
+        df = df.transpose()
+        df = df.loc[:, gene_ids]
+        condition_df = pd.concat([condition_df, df])
 
-ranked_genes = dict(zip(gene_symbols, expr_values))
-rnk = pd.Series(ranked_genes).sort_values(ascending=False)
+    other_condition_df = pd.DataFrame()
+    for other_condition in other_conditions:
+        path = f"{condition_parent_dir}/{other_condition}"
+        print(f"Processing Directory: {path}")
+        filenames = os.listdir(path)
+        random.shuffle(filenames)
 
-gsea_res = gp.prerank(rnk=rnk,
-                     gene_sets=['KEGG_2021_Human', 'Reactome_2022'],
-                     outdir=f'{condition_of_interest}_Enrichment',
-                     permutation_num=100)
+        for filename in filenames:
+            csv_path = f"{path}/{filename}"
+            df = pd.read_csv(csv_path, index_col=0)
+            df = np.log2(df + 1)
+            df = df.apply(get_zscores, axis=0)
+            df = df.apply(get_zscore_minmax, axis=0)
+            df = df.transpose()
+            df = df.loc[:, gene_ids]
+            other_condition_df = pd.concat([other_condition_df, df])
 
-print(gsea_res.res2d.head())
+    gene_pvals = []
+    for gene in gene_ids:
+        condition_vals = condition_df[gene].values
+        other_condition_vals = other_condition_df[gene].values
+        stat, pval = mannwhitneyu(condition_vals, other_condition_vals, alternative="two-sided")
+        gene_pvals.append(pval)
+
+    gene_adj_pvals = multipletests(gene_pvals, method="fdr_bh")[1]
+    mean_diff = condition_df.mean() - other_condition_df.mean()
+    gene_scores = -np.log10(gene_adj_pvals) * np.sign(mean_diff)
+
+    ensembl_ids = [x.split(".")[0] for x in gene_ids]
+    mg = mygene.MyGeneInfo()
+    query_result = mg.querymany(ensembl_ids, scopes='ensembl.gene', fields='symbol', species='human')
+    id_to_symbol = {item['query']: item.get('symbol', None) for item in query_result}
+    gene_symbols = list(filter(None, id_to_symbol.values()))
+
+    ranked_genes = dict(zip(gene_symbols, gene_scores))
+    rnk = pd.Series(ranked_genes).sort_values(ascending=False) 
+
+    gsea_res = gp.prerank(rnk=rnk,
+                        gene_sets=['KEGG_2021_Human', 'Reactome_2022', 'MSigDB_Hallmark_2020', 'GO_Biological_Process_2021', 'GO_Cellular_Component_2021', 'GO_Molecular_Function_2021'],
+                        outdir=f'Enrichments/{condition_of_interest}')
+
+    print(gsea_res.res2d.head())
+
+if __name__=="__main__":
+    main()
--- get_important_genes.py
+++ get_important_genes.py
@@ -1,95 +1,43 @@
-import h5py
-import mygene
 import numpy as np
+import os
 import pandas as pd
 import random
-from sklearn.decomposition import PCA
-from utils import clean_dose_unit, get_zscores, get_minmax, get_zscore_minmax
+from sklearn.feature_selection import f_classif
+from sklearn.preprocessing import LabelEncoder
+from utils import get_zscores, get_zscore_minmax
 
 def main():
-    gctx_file = "Data/annotated_GSE92742_Broad_LINCS_Level5_COMPZ_n473647x12328.gctx"
-    sample_condition_csv = "Conditions/Healthy/SRR16316900.csv"
-    gctx_fp = h5py.File(gctx_file, "r")
-    data_limit = 10000
-    num_genes = 1500
-
-    print("Loading GCTX Data")
-    data_idx = random.sample(range(0, len(gctx_fp["0/META/COL/id"])), data_limit)
-    data_idx.sort()
-    data_idx = np.array(data_idx)
-
-    ids = [s.decode('utf-8') for s in gctx_fp["0/META/COL/id"][data_idx]]
-    pert_dose_units = [clean_dose_unit(s) for s in gctx_fp["0/META/COL/pert_dose_unit"][data_idx]]
-    pert_time_units = [s.decode('utf-8') for s in gctx_fp["0/META/COL/pert_time_unit"][data_idx]]
-    pert_types = [s.decode('utf-8') for s in gctx_fp["0/META/COL/pert_type"][data_idx]]
-    gene_symbols = np.array([s.decode("utf-8") for s in gctx_fp["/0/META/ROW/pr_gene_symbol"]])
-
-    print("Matching CTL and TRT groups")
-    data_map = {}
-    for idx in range(data_limit):
-        id_parts = ids[idx].split(":")
+    gene_count = 5000
+    condition_parent_dir = "Conditions"
+    directory_list = ["Lupus", "Podoconiosis", "Primary_Sclerosing_Cholangitis", "Ulcerative_Colitis", "Shingles", "Sepsis", "Scleroderma", "MRSA_Bacteremia", "Crohns_Disease", "Acute_Pancreatitis", "Aneurysm", "Tuberculosis", "Acute_Myeloid_Leukemia", "Endocarditis", "Schistosomiasis", "Leprosy", "Amyotrophic_Lateral_Sclerosis", "Chronic_Myeloid_Leukemia", "Dengue", "Alzheimer", "Coronary_Artery_Disease", "COPD", "Breast_Cancer", "Crimean_Congo_Hemorrhagic_Fever"]
     
-        if len(id_parts) < 2:
-            continue
+    gene_expr_df = pd.DataFrame()
+    labels = []
+    for directory in directory_list:
+        path = f"{condition_parent_dir}/{directory}"
+        print(f"Processing Directory: {path}")
+        filenames = os.listdir(path)
+        random.shuffle(filenames)
 
-        condition = id_parts[0]
-        pert_type = pert_types[idx]
-        
-        if condition not in data_map:
-            data_map[condition] = {"ctl_idx": [], "trt_idx": []}
-        
-        if pert_type == "ctl_untrt" or pert_type == "ctl_vehicle":
-            data_map[condition]["ctl_idx"].append(idx)
+        for filename in filenames:
+            csv_path = f"{path}/{filename}"
+            df = pd.read_csv(csv_path, index_col=0)
+            df = np.log2(df + 1)
+            df = df.apply(get_zscores, axis=0)
+            df = df.apply(get_zscore_minmax, axis=0)
+            df = df.transpose()
+            gene_expr_df = pd.concat([gene_expr_df, df])
+            labels.append(directory)
 
-        elif pert_type == "trt_cp":
-            pert_dose_unit = pert_dose_units[idx]
-            pert_time_unit = pert_time_units[idx]
+    label_encoder = LabelEncoder()
+    labels_encoded = label_encoder.fit_transform(labels)
+    f, pvals = f_classif(gene_expr_df.values, labels_encoded)
+    gene_ranking = pd.Series(f, index=gene_expr_df.columns).sort_values(ascending=False)
+    top_genes = gene_ranking.head(gene_count).index.tolist()
 
-            if pert_dose_unit == "uM" and pert_time_unit == "h":
-                data_map[condition]["trt_idx"].append(idx)
-
-    print("Calculting Delta Exprs")
-    delta_exprs = []
-    for id, gene_data in data_map.items():
-        for ctl_idx in gene_data["ctl_idx"]:
-            ctl_expr = get_zscore_minmax(gctx_fp["0/DATA/0/matrix"][ctl_idx, :])
-            for trt_idx in gene_data["trt_idx"]:
-                trt_expr = get_zscore_minmax(gctx_fp["0/DATA/0/matrix"][trt_idx, :])
-                delta_exprs.append(trt_expr - ctl_expr)
-    
-    delta_exprs = np.array(delta_exprs)
-
-    print("Running PCA")
-    pca = PCA()
-    pca.fit(delta_exprs)
-    loadings = np.abs(pca.components_)
-    gene_scores = loadings[:20].sum(axis=0)
-    top_gene_indices = np.argsort(gene_scores)[::-1]
-    top_gene_symbols = gene_symbols[top_gene_indices]
-
-    print("Converting gene symbols to ENSEMBL")
-    mg = mygene.MyGeneInfo()
-    results = mg.querymany(top_gene_symbols, scopes='symbol', fields='ensembl.gene', species='human')
-    condition_df = pd.read_csv(sample_condition_csv, index_col=0)
-    condition_ensembl_ids = {ensembl_id.split(".")[0] for ensembl_id in condition_df.index}
-
-    important_genes = set()
-    results_idx = 0
-    while len(important_genes) < num_genes and results_idx < len(results):
-        result = results[results_idx]
-        if 'ensembl' in result:
-            if isinstance(result['ensembl'], list):
-                ensembl_id = result['ensembl'][0]['gene']
-            else:
-                ensembl_id = result['ensembl']['gene']
-            
-            if ensembl_id in condition_ensembl_ids:
-                important_genes.add((str(top_gene_symbols[results_idx]), ensembl_id))
-        results_idx += 1
-    
-    with open("Data/important_genes.csv", "w") as f:
-        for gene_symbol, ensembl_id in important_genes:
-            f.write(f"{gene_symbol},{ensembl_id}\n")
+    with open("Data/important_genes.txt", "w") as f:
+        for gene_id in top_genes:
+            f.write(f"{gene_id}\n")
 
 if __name__=="__main__":
     main()
